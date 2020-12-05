9cb13e9df6664d8edfd144f50d0fbcfc97d7e769
richardjzhang <rzhang@mathspace.com.au>
2020-12-06 10:44:35 +1100

    Add binary method of solving Day 5

--- 5/parttwo.js
+++ 5/parttwo.js
@@ -63,4 +63,23 @@ async function main() {
   }
 }
 
+// More efficient way to solve this problem
+async function efficient() {
+  const input = await readFile();
+  let seats = input
+    .split('\n')
+    .slice(0, -1)
+    .map((s) => s.replace(/B|R/g, 1).replace(/F|L/g, 0))
+    .map((s) => parseInt(s, 2));
+
+  // To find the missing seat in the unsorted list
+  // we can get the difference between the sum of all the seats
+  // and the sum of the consecutive integer from the minimum and the maximum
+  // The formula of the consecutive integer from A to B is (A+B)(A-B+1)/2
+  max = Math.max(...seats);
+  min = Math.min(...seats);
+  console.log(((max + min) * (max - min + 1)) / 2 - seats.reduce((a, b) => a + b));
+}
+
 main();
+efficient();
