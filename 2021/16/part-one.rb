a54f5ee8494ad67fca3dc9a2cd3efe77dd4ff048
Richard Zhang <rzhang@live.com.au>
2021-12-19 18:24:07 +1100

    Add Day 16

--- 2021/16/part-one.rb
+++ 2021/16/part-one.rb
@@ -1,61 +1,40 @@
 def read_file
-  file = File.open('sample-input.txt')
-  file.readlines.first.split ""
+  file = File.open('input.txt')
+  file.readline.chars.map{|c| c.hex.to_s(2).rjust(4, "0").chars}.flatten
 end
 
-def get_version packet
-  binary_to_hexa packet[0..2]
-end
-
-def get_packet_type_id packet
-  binary_to_hexa packet[3..5]
-end
-
-def get_length_type_id packet
-  binary_to_hexa packet[6]
-end
-
-def binary_to_hexa binary
-  binary.to_i(2)
-end
-
-def main
-  data = read_file
-  hexa_to_binary = {
-    "0" => "0000",
-    "1" => "0001",
-    "2" => "0010",
-    "3" => "0011",
-    "4" => "0100",
-    "5" => "0101",
-    "6" => "0110",
-    "7" => "0111",
-    "8" => "1000",
-    "9" => "1001",
-    "A" => "1010",
-    "B" => "1011",
-    "C" => "1100",
-    "D" => "1101",
-    "E" => "1110",
-    "F" => "1111"
-  }
-  
-  packet = ""
-  data.each do |char|
-    packet += hexa_to_binary[char]
-  end
-  
-  version = get_version packet
-  packet_type_id = get_packet_type_id packet
+def process_packet packet
+  version = packet.shift(3).join.to_i(2)
+  packet_type_id = packet.shift(3).join.to_i(2)
+  @version_total += version
 
   case packet_type_id
   when 4
-
+    while packet.shift == "1"
+      packet.shift(4)
+    end
+    packet.shift(4)
   else
-    length_type_id = get_length_type_id packet
-    
+    if packet.shift == "0"
+      len = packet.shift(15).join.to_i(2)
+      sub_packet = packet.shift(len)
+      until sub_packet.empty?
+        process_packet(sub_packet)
+      end
+    else
+      count = packet.shift(11).join.to_i(2)
+      count.times do
+        process_packet(packet)
+      end
+    end
   end
+end
 
+def main
+  @version_total = 0
+  packet = read_file
+  process_packet packet
+  puts @version_total
 end
 
 main
--- 2021/16/part-two.rb
+++ 2021/16/part-two.rb
@@ -1,11 +1,50 @@
 def read_file
-  file = File.open('sample-input.txt')
-  file.readlines.map { |x| x.chomp }
+  file = File.open('input.txt')
+  file.readline.chars.map{|c| c.hex.to_s(2).rjust(4, "0").chars}.flatten
 end
 
+def process_packet packet
+  version = packet.shift(3).join.to_i(2)
+  packet_type_id = packet.shift(3).join.to_i(2)
+
+  case packet_type_id
+  when 4
+    num = []
+    num += [*packet.shift(4)] while packet.shift == "1"
+    num += [*packet.shift(4)]
+    return [num.join.to_i(2), packet]
+  else
+    result = []
+    if packet.shift == "0"
+      len = packet.shift(15).join.to_i(2)
+      sub_packet = packet.shift(len)
+      until sub_packet.empty?
+        sub_result, sub_packet = process_packet(sub_packet)
+        result << sub_result
+      end
+    else
+      count = packet.shift(11).join.to_i(2)
+      count.times do
+        sub_result, packet = process_packet(packet)
+        result << sub_result
+      end
+    end
+
+    case packet_type_id
+    when 0; return [result.sum, packet]
+    when 1; return [result.reduce(&:*), packet]
+    when 2; return [result.min, packet]
+    when 3; return [result.max, packet]
+    when 5; return [result.reduce(&:>) ? 1 : 0, packet]
+    when 6; return [result.reduce(&:<) ? 1 : 0, packet]
+    when 7; return [result.reduce(&:==) ? 1 : 0, packet]
+    end
+  end
+end
 
 def main
-  data = read_file
+  packet = read_file
+  print process_packet(packet).first
 end
 
 main
